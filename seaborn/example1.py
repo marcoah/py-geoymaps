ee1c90ee8bd8e71fa07319b117035013eccd9f15
Marco Hernandez <marcoah@outlook.com>
2022-10-09 20:52:36 -0400

    Agregado ejemplo de histrograma con Seaborn

---
+++ seaborn/example1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+sns.set()
+
+np.random.seed(33)
+normal_data_a = np.random.normal(size = 500, loc = 100, scale = 10)
+normal_data_b = np.random.normal(size = 700, loc = 75, scale = 5)
+
+df_normal_a = pd.DataFrame(data = normal_data_a, columns=['score']).assign(group = 'A')
+df_normal_b = pd.DataFrame(data = normal_data_b, columns=['score']).assign(group = 'B')
+
+score_data = pd.concat([df_normal_a, df_normal_b])
+
+#print(score_data)
+
+sns.histplot(data=score_data, x='score')
