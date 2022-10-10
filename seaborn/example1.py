cc57ea6522980030e165252d0688e6d82f3e0110
Marco Hernandez <marcoah@outlook.com>
2022-10-09 22:30:46 -0400

    Arreglado salida del plot y segundo ejemplo

--- seaborn/example1.py
+++ seaborn/example1.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 sns.set()
 
@@ -11,8 +13,10 @@ normal_data_b = np.random.normal(size = 700, loc = 75, scale = 5)
 df_normal_a = pd.DataFrame(data = normal_data_a, columns=['score']).assign(group = 'A')
 df_normal_b = pd.DataFrame(data = normal_data_b, columns=['score']).assign(group = 'B')
 
-score_data = pd.concat([df_normal_a, df_normal_b])
+score_data = pd.concat([df_normal_a, df_normal_b], ignore_index=True)
 
-#print(score_data)
+print(score_data)
 
 sns.histplot(data=score_data, x='score')
+
+plt.show()
